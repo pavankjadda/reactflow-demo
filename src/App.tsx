a4546edadabd86166e77310f9f7067394efc5e3c
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2024-11-18 11:54:29 -0500

    fet(core): initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { ReactFlow } from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+
+function App() {
+  const initialNodes = [
+    { id: "1", position: { x: 0, y: 0 }, data: { label: "Concept" } },
+    { id: "2", position: { x: 0, y: 100 }, data: { label: "Branch Draft" } },
+    { id: "3", position: { x: 0, y: 200 }, data: { label: "SRC" } },
+    { id: "4", position: { x: 0, y: 300 }, data: { label: "PSO/IRB" } },
+  ];
+  const initialEdges = [
+    { id: "e1-2", source: "1", target: "2" },
+    { id: "e2-3", source: "2", target: "3" },
+    { id: "e3-4", source: "3", target: "4" },
+    { id: "e3-1", source: "3", target: "1" },
+    { id: "e4-2", source: "4", target: "2" },
+  ];
+
+  return (
+    <>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      </div>
+    </>
+  );
+}
+
+export default App;
